295a65327cf354ce61b22dead623eb42458834c6
andreluismoraes <andreluismoraes500@gmail.com>
2020-01-30 14:39:24 -0200

    colocando mais campos no userModel

--- model/userModel.js
+++ model/userModel.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const USERSchema = new mongoose.Schema({
     nomeUser: String,
     cpf: Number,
+    loginUser: String,
+    senhaUser: String,
+    funcaoUser: String, 
     emailUser: String,
     telefoneUser: String,
     dataNascimentoUser: Date,
