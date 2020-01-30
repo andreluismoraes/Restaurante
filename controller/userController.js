4b0f0d6efc432f08612953d3d2ef6c38fd68f9d0
andreluismoraes <andreluismoraes500@gmail.com>
2020-01-30 14:14:38 -0200

    index do userController

--- controller/userController.js
+++ controller/userController.js
@@ -4,13 +4,5 @@ module.exports = {
     async index(req, res){
         const user = await USER.find()
         return res.json(user)
-    },
-
-    async cadUser(req, res){
-        /**Usando o findOneAndUpdate e consultando o campo cpf como unico
-         * se cpf não existir ele cria um usuario caso contrário ele só altera os dados do usuario menos cpf
-        */
-        const user = {message: 'teste'}
-        return res.json(user)
     }
 }
