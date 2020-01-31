f9e3309b2ef1e8e2638bf04a6b55e6fd64677531
andreluismoraes <andreluismoraes500@gmail.com>
2020-01-31 11:51:56 -0200

    ajustando o model produto

--- model/produtoModel.js
+++ model/produtoModel.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 
 
 const ProdutoSchema = new mongoose.Schema({
-    nome: String,
-    valor: Number,
-    quantidade: Number,
-    validade: Date
+    nomeProduto: String,
+    valorProduto: Number,
+    quantidadeProduto: Number,
+    validadeProduto: Date
 })
 
 module.exports = mongoose.model('Produto', ProdutoSchema)
