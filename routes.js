b9fee30f9c33dcc28a2055ea62b657046b7d5a2c
andreluismoraes <andreluismoraes500@gmail.com>
2020-01-31 11:30:03 -0200

    ajustando as rotas dos produtos

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const USER = require('./controller/userController')
 const CLIENTE = require('./controller/clienteController')
+const PRODUTO  = require('./controller/produtoController')
 
 const routes = Router()
 
@@ -13,6 +14,11 @@ routes.post('/user', USER.store)
 routes.get('/cliente', CLIENTE.index)
 routes.get('/findCliente', CLIENTE.findCliente)
 routes.post('/cliente', CLIENTE.store)
+
+/**Usando as rotas dos produtos */
+routes.get('/produto', PRODUTO.index)
+routes.get('/findProduto', PRODUTO.findProduct)
+routes.post('/produto', PRODUTO.store)
 
 
 module.exports = routes
