48532b364a3b7d2fe1e08ecbd65e06691974a942
andreluismoraes <andreluismoraes500@gmail.com>
2020-02-10 21:48:05 -0200

    adicionando comentários para facilitar o entendimento

--- controller/pedidoController.js
+++ controller/pedidoController.js
@@ -2,6 +2,7 @@ const PEDIDO = require('../model/pedidoModel')
 
 module.exports = {
     async index(req, res){
+        /**procurar todos os pedidos e mostrar todos os dados */
         const pedido = await PEDIDO.find().populate('codigoProduto')
         res.json(pedido)
     },
@@ -9,6 +10,7 @@ module.exports = {
     async store(req, res){
         const {codigoPedido, codigoMesa, codigoProduto} = req.body
 
+        /**cadastrando um pedido */
         const pedido = await PEDIDO.findOneAndUpdate(
             //search
             {codigoPedido},
